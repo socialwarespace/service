f2c75268c0f8d9d3f3417d17e402fa0726a52978
David <slamvsem@gmail.com>
2018-12-21 23:27:10 +0700

    fix bot

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import database as data
 
 def auth():
     token = getter.get_token()
-    token = token[0:len(token)-2])#need to fix token.txt
+    token = token[0:len(token)-2] #need to fix token.txt
     vk = vk_api.VkApi(token=token)
     vk._auth_token()
     return vk
