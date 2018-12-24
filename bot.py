94994ffc542d1bc8b73669a02bda067de7e6128d
David <slamvsem@gmail.com>
2018-12-24 18:38:06 +0700

    fix

--- bot.py
+++ bot.py
@@ -268,7 +268,7 @@ def data_processing(id, pay, msg):
             if s != "":
                 photos = get_photos(directories, "main")
                 vk.method("messages.send", {"user_id": id, "message":s, "attachment": get_attachment(photos)})
-                vk.method("messages.send", {"user_id": id, "message":"Хотите, чтобы наш менеджер связался с Вами для уточнения информации по аренде автомобиля?\nОн напишет Вам в личные сообщения в самое ближайшее время.\nУбедитесь, что у Вас открыты личные сообщения.\n", "keyboard": key["connect"]})
+                vk.method("messages.send", {"user_id": id, "message":"Хотите, чтобы наш менеджер связался с Вами для уточнения информации по аренде автомобиля?\n\nОн напишет Вам в личные сообщения в самое ближайшее время.\n\nУбедитесь, что у Вас открыты личные сообщения.", "keyboard": key["connect"]})
         else:        
             vk.method("messages.send", {"user_id": id, "message": "К сожалению, по данным фильтрам результатов нет.", "keyboard": get_main_keyboard(id, connection)})     
     elif pay == "connect":
